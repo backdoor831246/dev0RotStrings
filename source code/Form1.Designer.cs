7a1ef0409afac826273c6098d2f7595fc6f2e074
dev0Rot <ivanovskiy.davidik@gmail.com>
2025-10-19 20:55:11 +0300

    Add files via upload

---
+++ source code/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace dev0RotStrings
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListView listViewStrings;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelFile;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCount;
+        private System.Windows.Forms.Label labelFilter;
+        private System.Windows.Forms.LinkLabel linkLabelUrl;
+        private System.Windows.Forms.Label labelCredit;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.listViewStrings = new System.Windows.Forms.ListView();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelFile = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labelFilter = new System.Windows.Forms.Label();
+            this.linkLabelUrl = new System.Windows.Forms.LinkLabel();
+            this.labelCredit = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(820, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(110, 28);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open file...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // listViewStrings
+            // 
+            this.listViewStrings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                                 | System.Windows.Forms.AnchorStyles.Left)
+                                 | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewStrings.HideSelection = false;
+            this.listViewStrings.Location = new System.Drawing.Point(12, 50);
+            this.listViewStrings.Name = "listViewStrings";
+            this.listViewStrings.Size = new System.Drawing.Size(918, 420);
+            this.listViewStrings.TabIndex = 1;
+            this.listViewStrings.UseCompatibleStateImageBehavior = false;
+            this.listViewStrings.DoubleClick += new System.EventHandler(this.listViewStrings_DoubleClick);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                                 | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilter.Location = new System.Drawing.Point(76, 16);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(680, 23);
+            this.txtFilter.TabIndex = 2;
+            this.txtFilter.TextChanged += new System.EventHandler(this.txtFilter_TextChanged);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCopy.Location = new System.Drawing.Point(12, 480);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(90, 28);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy selected";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.Location = new System.Drawing.Point(108, 480);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 28);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save all";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRefresh.Location = new System.Drawing.Point(204, 480);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(90, 28);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelFile,
+            this.toolStripStatusLabelCount});
+            this.statusStrip.Location = new System.Drawing.Point(0, 516);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(942, 22);
+            this.statusStrip.TabIndex = 6;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelFile
+            // 
+            this.toolStripStatusLabelFile.Name = "toolStripStatusLabelFile";
+            this.toolStripStatusLabelFile.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabelCount
+            // 
+            this.toolStripStatusLabelCount.Name = "toolStripStatusLabelCount";
+            this.toolStripStatusLabelCount.Size = new System.Drawing.Size(0, 17);
+            // 
+            // labelFilter
+            // 
+            this.labelFilter.AutoSize = true;
+            this.labelFilter.Location = new System.Drawing.Point(12, 19);
+            this.labelFilter.Name = "labelFilter";
+            this.labelFilter.Size = new System.Drawing.Size(44, 15);
+            this.labelFilter.TabIndex = 7;
+            this.labelFilter.Text = "Filter:";
+            // 
+            // linkLabelUrl
+            // 
+            this.linkLabelUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabelUrl.AutoSize = true;
+            this.linkLabelUrl.Location = new System.Drawing.Point(660, 487);
+            this.linkLabelUrl.Name = "linkLabelUrl";
+            this.linkLabelUrl.Size = new System.Drawing.Size(160, 15);
+            this.linkLabelUrl.TabIndex = 8;
+            this.linkLabelUrl.TabStop = true;
+            this.linkLabelUrl.Text = "https://github.com/backdoor831246";
+            // 
+            // labelCredit
+            // 
+            this.labelCredit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCredit.AutoSize = true;
+            this.labelCredit.Location = new System.Drawing.Point(820, 487);
+            this.labelCredit.Name = "labelCredit";
+            this.labelCredit.Size = new System.Drawing.Size(90, 15);
+            this.labelCredit.TabIndex = 9;
+            this.labelCredit.Text = "developed by ...";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(942, 538);
+            this.Controls.Add(this.labelCredit);
+            this.Controls.Add(this.linkLabelUrl);
+            this.Controls.Add(this.labelFilter);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.listViewStrings);
+            this.Controls.Add(this.btnOpen);
+            this.MinimumSize = new System.Drawing.Size(760, 380);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.BackColor = System.Drawing.Color.White;
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ source code/Form1.cs
@@ -0,0 +1,203 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dev0RotStrings
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] _args;
+
+        public Form1(string[] args)
+        {
+            _args = args ?? Array.Empty<string>();
+            InitializeComponent();
+            Text = "dev0RotStrings";
+            ShowUrlAndCredit();
+            SetupListView();
+            if (_args.Length > 0 && File.Exists(_args[0]))
+            {
+                LoadFile(_args[0]);
+            }
+        }
+
+        private void ShowUrlAndCredit()
+        {
+            linkLabelUrl.Text = "https://github.com/backdoor831246";
+            linkLabelUrl.Links.Clear();
+            linkLabelUrl.Links.Add(0, linkLabelUrl.Text.Length, linkLabelUrl.Text);
+            linkLabelUrl.LinkClicked += (s, e) =>
+            {
+                try { System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Link.LinkData?.ToString() ?? e.Link.ToString()) { UseShellExecute = true }); }
+                catch { /* fail silently */ }
+            };
+            labelCredit.Text = "developed by dev0Rot";
+        }
+
+        private void SetupListView()
+        {
+            listViewStrings.View = View.Details;
+            listViewStrings.FullRowSelect = true;
+            listViewStrings.Columns.Clear();
+            listViewStrings.Columns.Add("Offset", 90);
+            listViewStrings.Columns.Add("Type", 80);
+            listViewStrings.Columns.Add("String", 700);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using var ofd = new OpenFileDialog { Filter = "All files (*.*)|*.*", Title = "Open file to extract strings" };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                LoadFile(ofd.FileName);
+            }
+        }
+
+        private void LoadFile(string path)
+        {
+            try
+            {
+                toolStripStatusLabelFile.Text = path;
+                listViewStrings.Items.Clear();
+                var bytes = File.ReadAllBytes(path);
+                var ascii = ExtractAsciiStrings(bytes, 4);
+                var uni = ExtractUtf16LeStrings(bytes, 4);
+
+                foreach (var s in ascii)
+                {
+                    var item = new ListViewItem($"0x{s.Offset:X8}");
+                    item.SubItems.Add("ASCII");
+                    item.SubItems.Add(s.Value);
+                    listViewStrings.Items.Add(item);
+                }
+
+                foreach (var s in uni)
+                {
+                    var item = new ListViewItem($"0x{s.Offset:X8}");
+                    item.SubItems.Add("UTF-16LE");
+                    item.SubItems.Add(s.Value);
+                    listViewStrings.Items.Add(item);
+                }
+
+                toolStripStatusLabelCount.Text = $"{listViewStrings.Items.Count} strings";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке файла:\n{ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private IEnumerable<(int Offset, string Value)> ExtractAsciiStrings(byte[] data, int minLen)
+        {
+            var results = new List<(int, string)>();
+            var sb = new StringBuilder();
+            int start = -1;
+            for (int i = 0; i < data.Length; i++)
+            {
+                byte b = data[i];
+                if (b >= 0x20 && b <= 0x7E) // printable ASCII
+                {
+                    if (start < 0) start = i;
+                    sb.Append((char)b);
+                }
+                else
+                {
+                    if (start >= 0 && sb.Length >= minLen)
+                    {
+                        results.Add((start, sb.ToString()));
+                    }
+                    sb.Clear();
+                    start = -1;
+                }
+            }
+            if (start >= 0 && sb.Length >= minLen)
+                results.Add((start, sb.ToString()));
+            return results;
+        }
+
+        private IEnumerable<(int Offset, string Value)> ExtractUtf16LeStrings(byte[] data, int minLen)
+        {
+            var results = new List<(int, string)>();
+            int i = 0;
+            while (i + 1 < data.Length)
+            {
+                int start = i;
+                var sb = new StringBuilder();
+                while (i + 1 < data.Length)
+                {
+                    ushort ch = (ushort)(data[i] | (data[i + 1] << 8));
+                    if (ch >= 0x20 && ch <= 0x7E)
+                    {
+                        sb.Append((char)ch);
+                        i += 2;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (sb.Length >= minLen)
+                {
+                    results.Add((start, sb.ToString()));
+                }
+                i = Math.Max(i + 2, start + 1);
+            }
+            return results;
+        }
+
+        private void txtFilter_TextChanged(object sender, EventArgs e)
+        {
+            string filter = txtFilter.Text.ToLower();
+            listViewStrings.BeginUpdate();
+            foreach (ListViewItem item in listViewStrings.Items)
+            {
+                if (string.IsNullOrEmpty(filter) || item.SubItems[2].Text.ToLower().Contains(filter))
+                    item.ForeColor = System.Drawing.SystemColors.ControlText;
+                else
+                    item.ForeColor = System.Drawing.Color.Gray;
+            }
+            listViewStrings.EndUpdate();
+        }
+
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (listViewStrings.SelectedItems.Count == 0) return;
+            var sb = new StringBuilder();
+            foreach (ListViewItem it in listViewStrings.SelectedItems)
+            {
+                sb.AppendLine($"{it.SubItems[0].Text}\t{it.SubItems[1].Text}\t{it.SubItems[2].Text}");
+            }
+            Clipboard.SetText(sb.ToString());
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (listViewStrings.Items.Count == 0) return;
+            using var sfd = new SaveFileDialog { Filter = "Text file|*.txt", FileName = "strings.txt" };
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using var sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8);
+            foreach (ListViewItem it in listViewStrings.Items)
+            {
+                sw.WriteLine($"{it.SubItems[0].Text}\t{it.SubItems[1].Text}\t{it.SubItems[2].Text}");
+            }
+            MessageBox.Show("Сохранено.", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(toolStripStatusLabelFile.Text))
+                LoadFile(toolStripStatusLabelFile.Text);
+        }
+
+        private void listViewStrings_DoubleClick(object sender, EventArgs e)
+        {
+            if (listViewStrings.SelectedItems.Count == 0) return;
+            var text = listViewStrings.SelectedItems[0].SubItems[2].Text;
+            MessageBox.Show(text, "String", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
